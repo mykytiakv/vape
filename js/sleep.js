424bb2257d5131e8cba5eb61fb16d1c35d8ce976
mykytiakv <vasyamc@gmail.com>
2018-08-01 21:27:57 +0300

    add sleep window

---
+++ js/sleep.js
@@ -0,0 +1,57 @@
+idleTimer = null;
+idleState = false; // состояние отсутствия
+idleWait = 60000; // 1 минута
+ 
+$(document).ready( function(){
+  $(document).bind('mousemove keydown scroll', function(){
+    clearTimeout(idleTimer); // отменяем прежний временной отрезок
+    if(idleState == true){ 
+    }
+ 
+    idleState = false;
+    idleTimer = setTimeout(function(){ 
+      // Действия на отсутствие пользователя
+      $('.unload').removeClass('hidden');
+      idleState = true; 
+    }, idleWait);
+  });
+ 
+  $("body").trigger("mousemove"); // сгенерируем ложное событие, для запуска скрипта
+
+  $('.unload').on('click', function(e) {
+  	var unload = $('.unload')[0]
+  	var unOver = $('#unload-over')[0];
+  	var elem = e.target;
+  	if (elem === unload || elem === unOver) {
+  		$('.unload').addClass('hidden');
+  		idleState = false;
+  	}
+  });
+
+  // отправка формы
+
+  $('#unload-form').submit(function(e) {
+  	e.preventDefault();
+  	var form = $('#unload-form');
+  	var name = $(this).find('input[name="name"]')[0];
+  	var phone = $(this).find('input[name="phone"]')[0];
+  	var reg = /^((\+?3)?8)?((0\(\d{2}\)?)|(\(0\d{2}\))|(0\d{2}))\d{7}$/;
+
+  	if (name.value.length < 1) {
+  		$(name).addClass('is-invalid');
+  		e.stopPropagation();
+  	} else if (phone.value.match(reg) == null) {
+  		$(name).removeClass('is-invalid');
+  		$(phone).addClass('is-invalid');  		
+  	} else {
+  		$(name).removeClass('is-invalid');
+  		$(phone).removeClass('is-invalid');
+  		$('.unload').addClass('hidden');
+  		// submit form here
+
+  		// clear inputs
+  		name.value = '';
+  		phone.value = '';
+  	}  	
+  })
+});
